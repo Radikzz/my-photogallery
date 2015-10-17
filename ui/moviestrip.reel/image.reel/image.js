8a29a077166f0f9ebb99d1b5e2d656baaecf6ac5
Radikzz <noreply>
2015-10-17 21:32:07 +0000

    Update component ui/moviestrip.reel/image.reel/image.js

--- ui/moviestrip.reel/image.reel/image.js
+++ ui/moviestrip.reel/image.reel/image.js
@@ -3,14 +3,29 @@
  */
 var Component = require("montage/ui/component").Component;
 
-/**
- * @class Image
- * @extends Component
- */
-exports.Image = Component.specialize(/** @lends Image# */ {
-    constructor: {
-        value: function Image() {
-            this.super();
+exports.Image = Component.specialize( {
+
+    _src: {
+        value: null
+    },
+
+    src: {
+        set: function (value) {
+            if (value !== this._src) {
+                this._src = value;
+                this.needsDraw = true;
+            }
+        }
+    },
+
+    draw: {
+        value: function () {
+            if (this._src) {
+                this._element.style.backgroundImage = "url(" + this._src + ")";
+            } else {
+                this._element.style.backgroundImage = "url(assets/image/no-poster.jpg)";
+            }
         }
     }
+
 });
