a289af4e20b9554b9b50e69859197dba90bfb846
Radikzz <noreply>
2015-10-17 21:41:28 +0000

    Update component ui/moviestrip.reel/overlay.reel/overlay.js

--- ui/moviestrip.reel/overlay.reel/overlay.js
+++ ui/moviestrip.reel/overlay.reel/overlay.js
@@ -1,16 +1,12 @@
-/**
- * @module ui/moviestrip.reel/overlay.reel
- */
+
 var Component = require("montage/ui/component").Component;
 
-/**
- * @class Overlay
- * @extends Component
- */
-exports.Overlay = Component.specialize(/** @lends Overlay# */ {
+exports.Overlay = Component.specialize({
+
     constructor: {
         value: function Overlay() {
             this.super();
         }
     }
+
 });
