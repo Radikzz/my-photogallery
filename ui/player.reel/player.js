3fde8ad57340513c65517db75f61947c0d9c2733
Radikzz <noreply>
2015-10-17 21:13:25 +0000

    Add component player

---
+++ ui/player.reel/player.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/player.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Player
+ * @extends Component
+ */
+exports.Player = Component.specialize(/** @lends Player# */ {
+    constructor: {
+        value: function Player() {
+            this.super();
+        }
+    }
+});
