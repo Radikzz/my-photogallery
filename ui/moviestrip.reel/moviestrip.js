d3ff8fe53875cd18d7326db281a28b7e351a252c
Radikzz <noreply>
2015-10-17 21:12:51 +0000

    Add component moviestrip

---
+++ ui/moviestrip.reel/moviestrip.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/moviestrip.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Moviestrip
+ * @extends Component
+ */
+exports.Moviestrip = Component.specialize(/** @lends Moviestrip# */ {
+    constructor: {
+        value: function Moviestrip() {
+            this.super();
+        }
+    }
+});
