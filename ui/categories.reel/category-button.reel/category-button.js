d9e57a513c1d32ad1dc04c4380e802f325233e5a
Radikzz <noreply>
2015-10-17 21:14:42 +0000

    Add component category-button

---
+++ ui/categories.reel/category-button.reel/category-button.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/categories.reel/category-button.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class CategoryButton
+ * @extends Component
+ */
+exports.CategoryButton = Component.specialize(/** @lends CategoryButton# */ {
+    constructor: {
+        value: function CategoryButton() {
+            this.super();
+        }
+    }
+});
