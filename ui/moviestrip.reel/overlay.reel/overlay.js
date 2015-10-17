22ee0cc3d0de0dac021050f8f6f443da46916b43
Radikzz <noreply>
2015-10-17 21:17:24 +0000

    Add component overlay

---
+++ ui/moviestrip.reel/overlay.reel/overlay.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/moviestrip.reel/overlay.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Overlay
+ * @extends Component
+ */
+exports.Overlay = Component.specialize(/** @lends Overlay# */ {
+    constructor: {
+        value: function Overlay() {
+            this.super();
+        }
+    }
+});
